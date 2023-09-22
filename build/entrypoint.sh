096b4bd3d64216e28f7b9cae736678416fca00fa
Antoine Aflalo <Belphemur@users.noreply.github.com>
2023-09-22 17:20:30 -0400

    use CLI if any args are given, else be in noninteractive mode

--- build/entrypoint.sh
+++ build/entrypoint.sh
@@ -27,11 +27,12 @@ else
     # It also allows us to bind to the real ports :)
     socat TCP-LISTEN:25,fork TCP:127.0.0.1:1025 &
     socat TCP-LISTEN:143,fork TCP:127.0.0.1:1143 &
-
-    # Start protonmail
-    # Fake a terminal, so it does not quit because of EOF...
-    rm -f faketty
-    mkfifo faketty
-    cat faketty | /protonmail/proton-bridge --cli $@
-
+    
+    local command = "--cli $@"
+    if [ $# -eq 0 ]; then
+        # If no arguments are passed, start the bridge
+         command="-n"
+    fi
+    
+    /protonmail/proton-bridge $command
 fi
