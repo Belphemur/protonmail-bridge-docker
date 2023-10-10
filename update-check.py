0a67e54345ca55459f47ca9fec773a5c75a1cf28
Antoine Aflalo <Belphemur@users.noreply.github.com>
2023-10-10 18:45:56 -0400

    fix check update

--- update-check.py
+++ update-check.py
@@ -28,8 +28,9 @@ def check_version(directory, new_version):
         with open(f"{directory}/VERSION", "w") as f:
             f.write(new_version)
         # commit
-        result = os.system(f"git add {directory}/VERSION \
-            && git commit -m 'Bump {directory} version to {new_version}' \
+        result = os.system(f"git config --local user.email 'actions@github.com' \
+            && git config --local user.name 'GitHub Actions' \
+            && git add {directory}/VERSION \
             && git tag -f v{new_version}")
         if result != 0:
             print("Failed to commit the bump. Exiting")
@@ -37,7 +38,7 @@ def check_version(directory, new_version):
         if is_pull_request:
             print("Action triggered by pull request. Do not push.")
         else:
-            result = os.system("git push --tags")
+            result = os.system("git push origin master --tags")
             if result != 0:
                 print("Failed to push. Exiting")
                 exit(1)
